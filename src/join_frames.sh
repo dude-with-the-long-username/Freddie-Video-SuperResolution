b0079e605f383b78682f0b14ed86192b327047f3
Rajesh G <rajeshg3@outlook.com>
2020-02-17 17:01:41 +0530

    :pencil: fixed join script

--- src/join_frames.sh
+++ src/join_frames.sh
@@ -2,7 +2,7 @@
 
 SRC_DIR=$1
 DEST_FILE=$2
-PREFIX=""
-#ffmpeg -loop 1 -i ${SRC_DIR}/"${PREFIX}%d.bmp" $DEST_FILE
-ffmpeg -r 1/5 -i ${SRC_DIR}/"{PREFIX}%d.bmp" -c:v libx264 -vf fps=25 -pix_fmt yuv480p $DEST_FILE
+PREFIX="240p"
+ffmpeg -loop 1 -i ${SRC_DIR}/"${PREFIX}%d.png" $DEST_FILE
+#ffmpeg -r 1/5 -i ${SRC_DIR}/"${PREFIX}%d.png" -c:v libx264 -vf fps=25 -pix_fmt yuv480p $DEST_FILE
 #-c:v libx264 -pix_fmt yuv420p 
