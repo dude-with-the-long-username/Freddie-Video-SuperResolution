467893993d3af985032f745f3bb756337f33cb62
Rajesh G <rajeshg3@outlook.com>
2020-02-18 15:02:55 +0530

    :bug: fix joinframes script

--- src/join_frames.sh
+++ src/join_frames.sh
@@ -2,7 +2,8 @@
 
 SRC_DIR=$1
 DEST_FILE=$2
-PREFIX="240p"
-ffmpeg -loop 1 -i ${SRC_DIR}/"${PREFIX}%d.png" $DEST_FILE
+PREFIX=$3
+#ffmpeg -loop 1 -i ${SRC_DIR}/"${PREFIX}%d.png" $DEST_FILE
 #ffmpeg -r 1/5 -i ${SRC_DIR}/"${PREFIX}%d.png" -c:v libx264 -vf fps=25 -pix_fmt yuv480p $DEST_FILE
 #-c:v libx264 -pix_fmt yuv420p 
+ffmpeg -framerate 23.976 -i "${SRC_DIR}/${PREFIX}%d.png" $2
